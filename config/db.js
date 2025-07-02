00647f201711c2252476d7782b8e691cd1f1d2e2
Dev <devendra.rag@gmail.com>
2025-07-02 14:42:13 +0530

    db changes

--- config/db.js
+++ config/db.js
@@ -2,9 +2,29 @@ const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT, // include port
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+// const db = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME
+// });
+
+db.getConnection((err, connection) => {
+    if (err) {
+        console.error('MySQL connection failed:', err);
+    } else {
+        console.log('✅ Connected to MySQL');
+        connection.release();
+    }
 });
 
 module.exports = db;
