68dd46b599826a8fb94230e247b0600a7810a61c
Dev <devendra.rag@gmail.com>
2025-07-03 19:39:03 +0530

    Cors added

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const port = process.env.PORT || 3000;
 
 require('dotenv').config();
 
+const cors = require('cors');
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
